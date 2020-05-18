e4f157dde39c8e7c596f15ebb5cd9bc9b16b8de6
FrozenSky <517066422@qq.com>
2020-05-18 18:22:45 +0800

    [+] add description.

--- Demo/Demo00_Practice/ThinkPython_C03_1.py
+++ Demo/Demo00_Practice/ThinkPython_C03_1.py
@@ -1,3 +1,15 @@
+"""
+This module contains a code example related to
+
+Think Python, 2nd Edition
+by Allen Downey
+http://thinkpython2.com
+
+Copyright 2015 Allen Downey
+
+License: http://creativecommons.org/licenses/by/4.0/
+"""
+
 def right_justify(s):
     strLen = len(s)
     print(' ' * (70 - strLen) + s)
