12bd88aa84bcaf252c6c60e9b6e7c56d51d7c9af
FrozenSky <517066422@qq.com>
2023-05-20 01:02:40 +0800

    [+] Demo07

---
+++ Demo/Demo07/03_post.py
@@ -0,0 +1,14 @@
+import requests
+
+payload = {
+    'key1': 'value1',
+    'key2': 'value2'
+}
+
+r = requests.post("http://httpbin.org/post", data = payload)
+
+print(r.text)
+
+r = requests.post("http://httpbin.org/post", data = "ABC")
+
+print(r.text)
